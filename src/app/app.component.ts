c4bdf7f80bd9509c49edc7023696977fe2438bb4
Sivuyile Magutywa <s.magutywa@kernet.co.za>
2019-05-21 14:04:33 +0200

    added raphael svg library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as Raphael from 'raphael';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  hasHamburger = true;
+export class AppComponent implements AfterViewInit {
+
+
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    const paper = Raphael('canvas', 1280, 980);
+
+
+    paper.setStart();
+    const title = paper.text(18, 20, 'Home')
+      .attr({
+        'font-size': '14px',
+        cursor: 'pointer'
+      });
+    const boardBackground = paper.rect(0, 40, 1280, 980)
+      .attr({
+        fill: '#FFF',
+        stroke: 'none',
+        'stroke-width': 1
+      });
+
+    const board = paper.setFinish();
+    board
+      .mousemove((e) => {
+        console.log(e)
+      })
+      // .drag(() => {
+
+      // })
+      .hover(() => { }, () => {
+
+      })
+      .click(() => {
+        boardBackground.attr({
+          stroke: 'blue'
+        });
+      });
+  }
+
+
 }
